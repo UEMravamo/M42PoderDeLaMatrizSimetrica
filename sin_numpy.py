c885256a52585c9b8c71e65b0389aea1cc49e52b
dragoadri <dragoadri@gmail.com>
2024-12-09 19:51:57 +0100

    PEP8

--- sin_numpy.py
+++ sin_numpy.py
@@ -1,47 +1,45 @@
-def multiplicar_matrices(M1, M2):
-    n = len(M1) # dimension de la matriz
+def multiplicar_matrices(m1, m2):
+  """Multiplica dos matrices cuadradas."""
+  n = len(m1)  # Dimensión de la matriz
 
-    # crea matriz de dimension n llena de 0's
-    #aqui meteremos el resultado poco a poco
-    resultado = [[0] * n for _ in range(n)]
+  # Crea una matriz de dimensión n llena de ceros
+  resultado = [[0] * n for _ in range(n)]
 
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                #vamos llenando la matriz resultado y, recordamos que las multiplicaciones de matrices son FILAS X COLUMNAS
-                # De manera que A X B /= B X A
-                resultado[i][j] += M1[i][k] * M2[k][j]
-    return resultado
-
-def elevar_matriz_a_potencia(A, k):
-    n = len(A) # dimension de la matriz
-
-    #matriz identidad de la dimension de la matriz original
-    resultado = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
-
-    # Matriz identidad
-    '''
-    1 0 0
-    0 1 0
-    0 0 1
-    '''
+  for i in range(n):
+    for j in range(n):
+      for k in range(n):
+        # Vamos llenando la matriz resultado
+        # Recordamos que las multiplicaciones de matrices son FILAS x COLUMNAS
+        resultado[i][j] += m1[i][k] * m2[k][j]
+  return resultado
 
 
-    for _ in range(k): #for que se ejcuta k veces
+def elevar_matriz_a_potencia(a, k):
+  """Eleva una matriz cuadrada a una potencia k."""
+  n = len(a)  # Dimensión de la matriz
 
-        resultado = multiplicar_matrices(resultado, A)
-    return resultado
+  # Matriz identidad de la dimensión de la matriz original
+  identidad = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
+
+  resultado = identidad
+
+  for _ in range(k):  # Repite k veces
+    resultado = multiplicar_matrices(resultado, a)
+  return resultado
+
 
 # Matriz A
 A = [
-    [2, 1, -1],
-    [1, 0, 1],
-    [-1, 1, 2]
+  [2, 1, -1],
+  [1, 0, 1],
+  [-1, 1, 2]
 ]
 k = 4
+
+# Calcular A elevado a k
 Ak = elevar_matriz_a_potencia(A, k)
 
 # Imprimir el resultado
 print("Matriz A^4:")
 for fila in Ak:
-    print(fila)
+  print(fila)
