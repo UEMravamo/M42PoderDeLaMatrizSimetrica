df829e58923592caaa3d529713ee8f42e13ac638
Jaime22175478 <22175478@live.uem.es>
2024-12-11 22:24:45 +0100

    añadidos comentarios al algoritmo

--- exponenciacion_binaria.py
+++ exponenciacion_binaria.py
@@ -13,9 +13,28 @@ def generador_matrices(N):
 
 
 def exponenciacion_binaria(matriz, potencia):
-    potencia = bin(potencia)[2:]
-    rv = np.eye(len(matriz))
+    """ 
+    Primer paso, convertimos el exponente a binario, pongamos 9 como ejemplo.
+    9 -> 1001
+    Tras esto, empezamos recorriendo desde el bit menos significativo y cada uno
+    de ellos que tenga un uno sera el valor que usaremos para multiplicar y elevar.
+
+    1 -> 1 * X^(2^0) = 1 * X^1 
+    0 -> 0 * X^(2^1) = 0 * X^2
+    0 -> 0 * X^(2^2) = 0 * X^4
+    1 -> 1 * X^(2^3) = 1 * X^8
+
+    x^9 = X^1 * X^8
+
+    La variable pre almacena la potencia del indice de la exponencia binaria, 
+    haciendo que en cada iteración, se actualiza al cuadrado y si el bit
+    es 1 lo multiplicamos con rv. Con esto se consigue que el algoritmo
+    sea eficiente, con magnitud O(log(n)).
+    """
+    potencia = bin(potencia)[2:]  # bin(X) -> 0bXXXXX. Retiramos prefijo
+    rv = np.eye(len(matriz))  # Matriz identidad de una matrix NxN
     pre = matriz
+    # Orden inverso para empezar por el bit menos significativo.
     for bit in reversed(potencia):
         if bit == "1":
             rv = np.dot(rv, pre)
