0f131b45d108a97495e77717f72f655066144a33
user <22155937@live.uem.es>
2024-12-12 17:06:30 +0100

    cambios

--- test.py
+++ test.py
@@ -15,7 +15,9 @@ def matrix_sim(A,K):##cambio la n no hacia falta
         A = np.dot(A, A)
     
     return np.round(A, 2)#requisito ya que debemos redondear a 2 decimales 
-
+def matrix_sim2(A, K):#otra manera de hacerlo 
+    A = np.linalg.matrix_power(A, 2 ** K)
+    return np.round(A, 2)#Seguimos sin estar en la complejidad deseada 
 if __name__ == "__main__":
     
     print("Generar matriz:")
@@ -25,7 +27,14 @@ if __name__ == "__main__":
     print("Resultado: ")
     resultado = matrix_sim(A, K)
     print(resultado)
-   
+    print("Resultado 2:")
+    resultado2 =matrix_sim2(A,K)
+    print(resultado2)
+    if np.array_equal(resultado, resultado2):#comprobamos que el resultado es el mismo o no 
+        print("Los resultados son iguales.")
+    else:
+        print("Los resultados son diferentes.")
+    
 
     
 
