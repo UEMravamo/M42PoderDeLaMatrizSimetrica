a601d609dbac30996e25e3835f3178aae1e824fa
Jaime22175478 <22175478@live.uem.es>
2025-01-11 18:47:23 +0100

    pep8 + fix importacion

--- tests/test_negativos.py
+++ tests/test_negativos.py
@@ -1,6 +1,6 @@
 import numpy as np
 import time
-from tests.num_negativos import generador_matrices, generador_matrices_negativas, exponenciacion_binaria
+from num_negativos import generador_matrices, generador_matrices_negativas, exponenciacion_binaria
 
 
 def test_exponenciacion_binaria_matrices_negativas():
